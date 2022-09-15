20adddc62d7cda35de6c18f04d0e7bbe40d501c3
waysketch <waysketch@gmail.com>
2022-09-15 13:25:05 -0700

    logic updates for how maps are built. some type updates.

--- guardiana/src/components/Tv/Tv.tsx
+++ guardiana/src/components/Tv/Tv.tsx
@@ -4,54 +4,60 @@ import { useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import spriteSheet_max from '../../assets/characters/Max/base/max-sprite.png';
 
-class Background {
-    private pixels: { width: number, height: number };
-    private position: { x: number, y: number };
+class Map {
+    private pixels: { width: number, height: number }; // total pixels width and height
     private scale: number;
-    private bottom: any;
-    private top: any;
-    public playerStartPosition: [number, number];
+    private bottomImage: any; //
+    private topImage: any;
+    private cameraPosition: { x: number, y: number };
     private pixelOffset: { x: number, y: number };
-    public blocked: string[];
+    public grid: number[][];
+    public heroStartLocations: { name: string, x: number, y: number }[];
+    public enemyStartLocations: { name: string, x: number, y: number }[] | null[];
+    public npcStartLocations: { name: string, x: number, y: number }[] | null[];
 
     constructor(map: I.Map) {
-        this.scale = 3; // TODO GET THIS FROM SETTINGS
 
-        this.pixelOffset = map.pixelOffset;
+        // === Images === //
+        this.bottomImage = new Image()
+        this.topImage = new Image()
+        this.bottomImage.src = map.imageBottom
+        this.topImage.src = map.imageTop
 
-        this.blocked = map.blocked;
+        this.heroStartLocations = map.heroStartLocations
+        this.enemyStartLocations = map.enemyStartLocations
+        this.npcStartLocations = map.enemyStartLocations
 
-        this.playerStartPosition = map.startingLocation;
+        this.grid = map.grid
 
-        this.position = {
-            x: 0 - this.playerStartPosition[0] * 24,
-            y: 0 - this.playerStartPosition[1] * 24
-        }
+        this.scale = 3 // TODO GET THIS FROM SETTINGS
+
+        this.pixelOffset = map.pixelOffset
+
+        this.grid = map.grid
+
+        this.cameraPosition = map.cameraStartLocation
 
         this.pixels = {
-            width: map.grid[0] * 24,
-            height: map.grid[1] * 24
+            width: map.mapDimentions.x * I.PIXEL.BLOCK,
+            height: map.mapDimentions.y * I.PIXEL.BLOCK
         }
 
-        this.bottom = new Image();
-        this.top = new Image();
-        this.bottom.src = map.imageBottom;
-        this.top.src = map.imageTop
     }
 
     move(direction: Direction) {
         switch (direction) {
             case Direction.DOWN:
-                this.position.y -= (24 * this.scale);
+                this.cameraPosition.y -= (I.PIXEL.BLOCK * this.scale);
                 break;
             case Direction.UP:
-                this.position.y += (24 * this.scale);
+                this.cameraPosition.y += (I.PIXEL.BLOCK * this.scale);
                 break;
             case Direction.LEFT:
-                this.position.x -= (24 * this.scale);
+                this.cameraPosition.x -= (I.PIXEL.BLOCK * this.scale);
                 break;
             case Direction.RIGHT:
-                this.position.x += (24 * this.scale);
+                this.cameraPosition.x += (I.PIXEL.BLOCK * this.scale);
                 break;
             default:
                 break;
@@ -66,28 +72,16 @@ class Background {
         ctx.imageSmoothingEnabled = false;
 
         ctx.drawImage(
-            this.bottom,
+            this.bottomImage,
             this.pixelOffset.x, // TODO: Why is the map loading 4 pixels off?
             this.pixelOffset.y,
             this.pixels.height,
             this.pixels.width,
-            this.position.x,
-            this.position.y,
+            this.cameraPosition.x,
+            this.cameraPosition.y,
             this.pixels.width * this.scale,
             this.pixels.height * this.scale
         );
-
-        // ctx.drawImage(
-        //     this.top,
-        //     -4, // TODO: Why is the map loading 4 pixels off?
-        //     0,
-        //     this.pixels.height,
-        //     this.pixels.width,
-        //     this.position.x,
-        //     this.position.y,
-        //     this.pixels.width * this.scale,
-        //     this.pixels.height * this.scale
-        // );
     }
 
     drawMapTop() {
@@ -96,17 +90,22 @@ class Background {
         ctx.imageSmoothingEnabled = false;
 
         ctx.drawImage(
-            this.top,
+            this.topImage,
             this.pixelOffset.x, // TODO: Why is the map loading 4 pixels off?
             this.pixelOffset.y,
             this.pixels.height,
             this.pixels.width,
-            this.position.x,
-            this.position.y,
+            this.cameraPosition.x,
+            this.cameraPosition.y,
             this.pixels.width * this.scale,
             this.pixels.height * this.scale
         );
     }
+
+    cameraToTarget(targetLocation: { x: number, y: number }) {
+        this.cameraPosition = targetLocation;
+    }
+
 }
 
 enum Direction {
@@ -116,33 +115,45 @@ enum Direction {
     DOWN
 }
 
+class Item {
+    constructor() {
+
+    }
+}
+
 class Character {
-    public position: { x: number, y: number };
+    public positionOnTV: { x: number, y: number };
     private size: any;
     private sprite: any;
     private spriteGrid: any;
     private lastUpdate: any;
     private scale: number; //TODO pull from settings
-    public square: [number, number];
+    public currentLocation: { x: number, y: number };
+    public currentSquareID: string;
 
-    constructor() {
+    constructor(spawnLocation: { name: string, x: number, y: number }) {
         this.scale = 3;
 
-        this.position = {
-            x: 0 * 24 * this.scale,
-            y: 0 * 24 * this.scale
+        this.positionOnTV = {
+            x: spawnLocation.x * I.PIXEL.BLOCK * this.scale,
+            y: spawnLocation.y * I.PIXEL.BLOCK * this.scale
         } // your location on the 576x480 canvus 
 
-        this.square = [0, 0]; // You place on the map.
+        this.currentLocation = {
+            x: spawnLocation.x,
+            y: spawnLocation.y
+        }; // You place on the map.
+
+        this.currentSquareID = `${this.currentLocation.x}${this.currentLocation.y}`;
 
         this.size = {
-            width: 24,
-            height: 24
+            width: I.PIXEL.BLOCK,
+            height: I.PIXEL.BLOCK
         } // the size of your sprite from left to right
 
         this.spriteGrid = {
-            x: 24,
-            y: 96
+            x: 0,
+            y: 0
         }; // the full size of the sprite grid
 
         this.lastUpdate = Date.now();
@@ -152,99 +163,112 @@ class Character {
     }
 
     face(direction: Direction) {
-        switch (direction) {
-            case Direction.DOWN:
-                this.spriteGrid.y = 0;
-                break;
-            case Direction.UP:
-                this.spriteGrid.y = 24;
-                break;
-            case Direction.LEFT:
-                this.spriteGrid.y = 48;
-                break;
-            case Direction.RIGHT:
-                this.spriteGrid.y = 72;
-                break;
-            default:
-                console.log('Bad direction detected.');
-                break;
+        try {
+            switch (direction) {
+                case Direction.DOWN:
+                    this.spriteGrid.y = 0;
+                    break;
+                case Direction.UP:
+                    this.spriteGrid.y = I.PIXEL.BLOCK;
+                    break;
+                case Direction.LEFT:
+                    this.spriteGrid.y = I.PIXEL.BLOCK * 2;
+                    break;
+                case Direction.RIGHT:
+                    this.spriteGrid.y = I.PIXEL.BLOCK * 3;
+                    break;
+                default:
+                    console.log('Bad direction detected.');
+                    break;
+            }
+        } finally {
+            this.Update();
         }
     }
 
+    updateSquareID() {
+        this.currentSquareID = `${this.currentLocation.x}${this.currentLocation.y}`;
+    }
+
     move(direction: Direction) {
-        switch (direction) {
-            case Direction.DOWN:
-                this.square[1]++;
-                this.position.y += 24 * this.scale;
-                console.log('pos update');
-                break;
-            case Direction.UP:
-                this.square[1]--;
-                this.position.y -= 24 * this.scale;
-                console.log('pos update');
-                break;
-            case Direction.LEFT:
-                this.square[0]--;
-                this.position.x -= 24 * this.scale;
-                break;
-            case Direction.RIGHT:
-                this.square[0]++;
-                this.position.x += 24 * this.scale;
-                break;
-            default:
-                console.log('Bad direction detected.');
-                break;
+        try {
+            switch (direction) {
+                case Direction.DOWN:
+                    this.currentLocation.y++;
+                    this.positionOnTV.y += I.PIXEL.BLOCK * this.scale;
+                    break;
+                case Direction.UP:
+                    this.currentLocation.y--;
+                    this.positionOnTV.y -= I.PIXEL.BLOCK * this.scale;
+                    break;
+                case Direction.LEFT:
+                    this.currentLocation.x--;
+                    this.positionOnTV.x -= I.PIXEL.BLOCK * this.scale;
+                    break;
+                case Direction.RIGHT:
+                    this.currentLocation.x++;
+                    this.positionOnTV.x += I.PIXEL.BLOCK * this.scale;
+                    break;
+                default:
+                    console.log('Bad direction detected.');
+                    break;
+            }
+        } finally {
+            this.Update();
         }
+
     }
 
     draw() {
         if (this.lastUpdate + 500 <= Date.now()) {
             this.lastUpdate = Date.now();
-            if (this.spriteGrid.x === 24) {
+            if (this.spriteGrid.x === I.PIXEL.BLOCK) {
                 this.spriteGrid.x = 0;
             } else {
-                this.spriteGrid.x = 24;
+                this.spriteGrid.x = I.PIXEL.BLOCK;
             };
         }
 
         const spriteFinalSizeInPixels = {
-            x: 24 * this.scale,
-            y: 24 * this.scale
+            x: I.PIXEL.BLOCK * this.scale,
+            y: I.PIXEL.BLOCK * this.scale
         }
 
         let ctx: any = document.getElementById('layer-0');
         ctx = ctx.getContext('2d');
         ctx.imageSmoothingEnabled = false;
-        ctx.drawImage(this.sprite,
+        ctx.drawImage(
+            this.sprite,
             this.spriteGrid.x,
             this.spriteGrid.y,
             this.size.width,
             this.size.height,
-            this.position.x,
-            this.position.y,
+            this.positionOnTV.x,
+            this.positionOnTV.y,
             spriteFinalSizeInPixels.x,
             spriteFinalSizeInPixels.y);
     }
 
-    update() {
+    Update() {
         this.draw();
+        this.updateSquareID();
     }
 }
 
 export default function Tv() {
     let mounted: boolean = true;
     const map: I.Map = useSelector((state: { map: I.Map }) => state.map);
-    const background = new Background(map);
-    const player = new Character();
+    const Background = new Map(map);
+    const Player = new Character(Background.heroStartLocations[0]);
 
     const Update = () => {
         if (!mounted) { return };
 
         let ctx: any = document.getElementById('layer-0')
         ctx = ctx.getContext('2d');
-        background.drawMapBottom();
-        player.draw();
-        background.drawMapTop();
+        Background.drawMapBottom();
+        Player.draw();
+        Background.drawMapTop();
         //}
 
         requestAnimationFrame(Update);
@@ -255,91 +279,93 @@ export default function Tv() {
         switch (keyCode) {
             case 87: //W //UP
                 {
-                    player.face(Direction.UP);
+                    Player.face(Direction.UP);
 
-                    const nextSquare = `${player.square[0]}${player.square[1] - 1}`;
-                    if (background.blocked.includes(nextSquare)) {
-                        console.log(`boop: ${nextSquare} is blocked`);
+                    // check for blocks
+
+                    const nextSquare: number = Background.grid[Player.currentLocation.y - 1][Player.currentLocation.x];
+                    if (nextSquare <= 7) {
+                        console.log(`boop`);
+                        return;
+                    };
+
+                    // check if camera update is needed
+                    if (Player.currentLocation.y <= 72) {
+                        Background.move(Direction.UP);
+                        Player.currentLocation.y--;
+                        Player.Update();
+                    } else {
+                        Player.move(Direction.UP);
+                    };
+
+                    console.table(`Current Block: ${Player.currentLocation.x}${[Player.currentLocation.y]}`);
+                    break;
+                }
+            case 83: //S
+                {
+                    Player.face(Direction.DOWN);
+
+                    const nextSquare: number = Background.grid[Player.currentLocation.y + 1][Player.currentLocation.x];
+                    if (nextSquare <= 7) {
+                        console.log(`boop`);
                         return;
                     };
 
                     // camera update
-                    // console.table(player.position);
-                    // console.log('currentSquare: ' + player.square);
-                    if (player.position.y <= 72) {
-                        background.move(Direction.UP);
-                        player.square[1]--;
-                        player.draw();
-                    } else {
-                        player.move(Direction.UP);
+                    if (Player.positionOnTV.y >= 300) {
+                        // move camera with player.
+                        Background.move(Direction.DOWN);
+                        Player.currentLocation.y++;
+                        Player.Update();
+                    }
+                    else {
+                        Player.move(Direction.DOWN);
                     };
 
-                    console.table(player.position);
-                    console.log('currentSquare: ' + player.square);
-
+                    console.table(`Current Block: ${Player.currentLocation.x}${[Player.currentLocation.y]}`);
                     break;
                 }
-            case 83: //S
-                player.face(Direction.DOWN);
-
-                const nextSquare = `${player.square[0]}${player.square[1] + 1}`;
-                if (background.blocked.includes(nextSquare)) {
-                    console.log(`boop: ${nextSquare} is blocked`);
-                    return;
-                };
-
-                // camera update
-                if (player.position.y >= 300) {
-                    // move camera with player.
-                    background.move(Direction.DOWN);
-                    player.square[1]++;
-                    player.draw();
-                }
-                else {
-                    player.move(Direction.DOWN);
-                };
-
-                console.table(player.position);
-                console.log('currentSquare: ' + player.square);
-
-                break;
             case 65: //A
                 {
-                    player.face(Direction.LEFT);
+                    Player.face(Direction.LEFT);
 
-                    const nextSquare = `${player.square[0] - 1}${player.square[1]}`;
-                    if (background.blocked.includes(nextSquare)) {
-                        console.log(`boop: ${nextSquare} is blocked`);
+                    const nextSquare: number = Background.grid[Player.currentLocation.y][Player.currentLocation.x - 1];
+                    if (nextSquare <= 7) {
+                        console.log(`boop`);
                         return;
                     };
-                    if (player.position.x <= 72) {
-                        background.move(Direction.RIGHT);
-                        player.square[0]++;
-                        player.draw();
+
+                    // check if we need to move the camera
+                    if (Player.positionOnTV.x <= 72) {
+                        Background.move(Direction.RIGHT);
+                        Player.currentLocation.x--;
+                        Player.Update();
                     } else {
-                        player.move(Direction.LEFT);
+                        Player.move(Direction.LEFT);
                     }
+
+                    console.table(`Current Block: ${Player.currentLocation.x}${[Player.currentLocation.y]}`);
                     break;
                 }
-
             case 68: //D
                 {
-                    player.face(Direction.RIGHT);
+                    Player.face(Direction.RIGHT);
 
-                    const nextSquare = `${player.square[0] + 1}${player.square[1]}`;
-                    if (background.blocked.includes(nextSquare)) {
-                        console.log(`boop: ${nextSquare} is blocked`);
+                    const nextSquare: number = Background.grid[Player.currentLocation.y][Player.currentLocation.x + 1];
+                    if (nextSquare <= 7) {
+                        console.log(`boop`);
                         return;
                     };
 
-                    if (player.position.x >= 432) {
-                        background.move(Direction.LEFT);
-                        player.square[0]++;
-                        player.draw();
+                    if (Player.positionOnTV.x >= 432) {
+                        Background.move(Direction.LEFT);
+                        Player.currentLocation.x++;
+                        Player.Update();
                     } else {
-                        player.move(Direction.RIGHT);
+                        Player.move(Direction.RIGHT);
                     }
 
+                    console.table(`Current Block: ${Player.currentLocation.x}${[Player.currentLocation.y]}`);
                     break;
                 }
             default:
--- guardiana/src/utils/types.ts
+++ guardiana/src/utils/types.ts
@@ -6,6 +6,10 @@ export enum ACTIONS {
     TEST = 'TEST'
 }
 
+export enum PIXEL {
+    BLOCK = 24 // the size of each block in the game as pixels
+}
+
 
 // ================== //
 // === INTERFACES === //
@@ -68,10 +72,52 @@ export interface Item {
 }
 
 export interface Map {
+    /* === Layer 0 for the map. This will be behind everything.
+    NOTE this is the only layer that can be a jpg. === */
+    // example: "/asset/map/city/Loom.png";
     imageBottom: string;
+
+    // === Layer 8 for the map. This will be over everything. Including over the player === //
     imageTop: string;
-    blocked: string[];
-    grid: [number, number];
-    startingLocation: [number, number];
-    pixelOffset: {x: number, y: number};
+
+    // === Map size in squares === //
+    // example: { x: 10, y: 20 } would make a 240px by 480px map
+    mapDimentions: { x: number, y: number };
+
+    // === the block grid === //
+    //  grid: [
+    //      [0,0,0,0];
+    //      [0,9,9,0];
+    //      [0,9,9,0];
+    //      [0,0,0,0];
+    // ]
+    // use case: map.grid[1][3] === 0? doneMove() : move();
+    // KEY:
+    // 0 = blocked
+    // 9 = open
+    // 8 = open FRIENDLY
+    // 7 = blocking WATER
+    // 6 = blocked SHRUBS or OBSTRUCTION (can fly over)
+    // 5 = open but if you stop an action happens (interactionIfStoppedID)
+    // 4 = blocked
+    // 3 = blocked NON_FRIENDLY
+    // 2 = blocked
+    // 1 = blocked
+    grid: number[][];
+
+    // === This shifts the map but not the grid the player stands on. === //
+    pixelOffset: { x: number, y: number };
+
+    // === The location of the camera when the game loads === //
+    // NOTE: this may be removed in the future when I get focus to work. 
+    cameraStartLocation: { x: number, y: number };
+
+    // === The location of party assets. === //
+    heroStartLocations: { name: string, x: number, y: number }[];
+
+    // === The location of NPC assets. === //
+    npcStartLocations: { name: string, x: number, y: number }[] | null[];
+
+    // === The location of Enemy assets === //
+    enemyStartLocations: { name: string, x: number, y: number }[] | null[];
 }
